355c60f11dd9afc316da5731aea73c33ce5fff8d
Meg <enkhchimeg.e02@gmail.com>
2026-02-03 23:07:39 +0900

    Adding User Input Validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,18 @@
 const User = require("../models/user.model");
 const authUtils = require("../util/authentication");
+const validation = require("../util/validation"); // Make sure this is imported
 
 function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
 async function signup(req, res, next) {
+  if ((!validation.userDetailsAreValid(req.body.email, req.body.password, req.body.fullname, req.body.street, req.body.postal, req.body.city)
+    || !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"]))) {
+    res.redirect("/signup");
+    return;
+  }
+  
   const user = new User(
     req.body.email,
     req.body.password,
@@ -14,16 +21,20 @@ async function signup(req, res, next) {
     req.body.postal,
     req.body.city,
   );
- try {
-await user.signup();
-} catch (error) {
-  next(error);
-  return;
+  
+  try {
+    const existsAlready = await user.existsAlready();
+    if (existsAlready) {
+      res.redirect("/signup");
+      return;
+    }
+    await user.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  res.redirect("/login");
 }
-res.redirect("/login");
-}
-
-
 
 function getLogin(req, res) {
   res.render("customer/auth/login");
@@ -32,10 +43,10 @@ function getLogin(req, res) {
 async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
   let existingUser;
+  
   try {
- existingUser = await user.getUserWithSameEmail();
-  }
-  catch (error){
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
     next(error);
     return;
   }
@@ -44,18 +55,20 @@ async function login(req, res, next) {
     res.redirect("/login");
     return;
   }
+  
   const passwordIsValid = await user.hasMatchingPassword(existingUser.password);
   if (!passwordIsValid) {
     res.redirect("/login");
     return;
   }
-authUtils.createUserSession(req, existingUser, function () {
+  
+  authUtils.createUserSession(req, existingUser, function () {
     res.redirect("/");
   });
 }
 
-function logout(req,res){
-  authUtil.destroyUserAuthSession(req);
+function logout(req, res) {
+  authUtils.destroyUserAuthSession(req);
   res.redirect("/login");
 }
 
@@ -65,53 +78,4 @@ module.exports = {
   getLogin,
   login,
   logout
-};
-
-const User = require("../models/user.model");
-const authUtils = require("../util/authentication");
-
-function getSignup(req, res) {
-  res.render("customer/auth/signup");
-}
-
-async function signup(req, res) {
-  const user = new User(
-    req.body.email,
-    req.body.password,
-    req.body.fullname,
-    req.body.street,
-    req.body.postal,
-    req.body.city,
-  );
-  await user.signup();
-  res.redirect("/login");
-}
-
-function getLogin(req, res) {
-  res.render("customer/auth/login");
-}
-
-async function login(req, res) {
-  const user = new User(req.body.email, req.body.password);
-  const existingUser = await user.getUserWithSameEmail();
-
-  if (!existingUser) {
-    res.redirect("/login");
-    return;
-  }
-  const passwordIsValid = await user.hasMatchingPassword(existingUser.password);
-  if (!passwordIsValid) {
-    res.redirect("/login");
-    return;
-  }
-authUtils.createUserSession(req, existingUser, function () {
-    res.redirect("/");
-  });
-}
-
-module.exports = {
-  getSignup,
-  signup,
-  getLogin,
-  login
 };
--- models/user.model.js
+++ models/user.model.js
@@ -19,6 +19,15 @@ class User {
     return db.getDb().collection('users').findOne({ email: this.email });
   }
 
+async existsAlready() {
+  const existingUser = await this.getUserWithSameEmail();
+  if (existingUser) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
 
