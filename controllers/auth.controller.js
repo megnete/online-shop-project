ddd796ba603bbc8c9203a693e73015eaf3afcd9b
Meg <enkhchimeg.e02@gmail.com>
2026-02-05 21:58:06 +0900

    edited:   controllers/auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -17,17 +17,31 @@ async function signup(req, res, next) {
     city: req.body.city,
   };
 
-  if (!validation.userDetailsAreValid(req.body.email, req.body.password, req.body.fullname, req.body.street, req.body.postal, req.body.city)
-    || !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])) {
-    sessionFlash.flashDataToSession(req, {
-      errorMessage: "Please check your input. Passwords must be at least 6 characters long and emails must match.",
-      ...enteredData
-    }, function () {
-      res.redirect("/signup");
-    });
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city,
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+  ) {
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          "Please check your input. Passwords must be at least 6 characters long and emails must match.",
+        ...enteredData,
+      },
+      function () {
+        res.redirect("/signup");
+      },
+    );
     return;
   }
-  
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -36,26 +50,30 @@ async function signup(req, res, next) {
     req.body.postal,
     req.body.city,
   );
-  
+
   try {
     const existsAlready = await user.existsAlready();
 
     if (existsAlready) {
-      sessionFlash.flashDataToSession(req, {
-        errorMessage: "User with this email already exists!",
-        ...enteredData,
-      }, function () {
-        res.redirect("/signup");
-      });
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: "User with this email already exists!",
+          ...enteredData,
+        },
+        function () {
+          res.redirect("/signup");
+        },
+      );
       return;
     }
-    
+
     await user.signup();
   } catch (error) {
     next(error);
     return;
   }
-  
+
   res.redirect("/login");
 }
 
@@ -66,7 +84,7 @@ function getLogin(req, res) {
 async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
   let existingUser;
-  
+
   try {
     existingUser = await user.getUserWithSameEmail();
   } catch (error) {
@@ -79,14 +97,14 @@ async function login(req, res, next) {
     email: req.body.email,
     password: req.body.password,
   };
-  
+
   if (!existingUser) {
     sessionFlash.flashDataToSession(req, sessionErrorData, function () {
       res.redirect("/login");
     });
     return;
   }
-  
+
   const passwordIsValid = await user.hasMatchingPassword(existingUser.password);
   if (!passwordIsValid) {
     sessionFlash.flashDataToSession(req, sessionErrorData, function () {
@@ -94,7 +112,7 @@ async function login(req, res, next) {
     });
     return;
   }
-  
+
   authUtils.createUserSession(req, existingUser, function () {
     res.redirect("/");
   });
@@ -110,5 +128,5 @@ module.exports = {
   signup,
   getLogin,
   login,
-  logout
+  logout,
 };
